9720fa597c84d7caa2139961bc6879fc1306f4f3
Matias Negrete <49992569+Enel0@users.noreply.github.com>
2025-06-25 17:34:16 -0400

    Use JWT for auth

--- backend/src/utils/jwtUtils.js
+++ backend/src/utils/jwtUtils.js
@@ -1,9 +1,13 @@
-export const generarToken = (usuario) => {
-  const payload = {
-    id: usuario._id,
-    nombre: usuario.nombre,
-    email: usuario.email,
-    rol: usuario.rol,
-  };
-  return btoa(JSON.stringify(payload)); // Codifica el payload en Base64
-};
+import jwt from "jsonwebtoken";
+
+export const generarToken = (usuario) =>
+  jwt.sign(
+    {
+      id: usuario._id,
+      nombre: usuario.nombre,
+      email: usuario.email,
+      rol: usuario.rol,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRATION || "1h" }
+  );
